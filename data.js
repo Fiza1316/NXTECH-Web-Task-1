10db67f0adb8e750c5cd11eddf0b3cf292a6a828
Fiza1316 <140894764+Fiza1316@users.noreply.github.com>
2023-07-31 11:37:03 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,34 @@
+
+let songs = [
+        {
+            name: ' Jashan-E-Baharaa ',
+            path: '/music/Song 1.mp3',
+            artist: 'A.R. Rehman',
+            cover: '/images/cover 1.png',
+        },
+        {
+            name: 'Ishq wala Love',
+            path: '/music/Song 2.mp3',
+            artist: 'Shekhar Ravjiani , Salim Merchant , Neeti Mohan',
+            cover: '/images/cover 2.png'
+        },
+        {
+            name: 'Mere Dholna Sun',
+            path: '/music/Song 3.mp3',
+            artist: 'Shreya Ghosal & M.G. Shree Kumar',
+            cover: '/images/cover 3.png'
+        },
+        {
+            name: 'Tujhse Mehka-Mehka Sa ',
+            path: '/music/Song 4.mp3',
+            artist: 'Prashant Soni',
+            cover: '/images/cover 4.png'
+        
+        },
+        {
+            name: 'Jaane Kyuu',
+            path: '/music/Song 5.mp3',
+            artist: 'Vishal Dadlani',
+            cover: '/images/cover 5.png'
+        }
+           ]
